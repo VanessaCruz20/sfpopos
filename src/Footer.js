396805ac2e58fa4f637a329c90b4f65c168815b2
VanessaCruz20 <vanessa.cruz@students.dominican.edu>
2020-11-13 17:37:58 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <small>Mitchell Hudson copyright 2019</small>
+    </div>
+  );
+}
+
+export default Footer
